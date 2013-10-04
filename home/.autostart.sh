ae76ee8e83a7441d34076ab34edd2f95b56488f1
Caleb Everett <everett.caleb@gmail.com>
2013-10-04 10:36:12 -0400

    autostart: Add file check to system autostart, make executable

--- home/.autostart.sh
+++ home/.autostart.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+system=~/.autostart.system.sh
+
 #:: start programs
 xrandr --auto # screen setup
 compton &     # composition
 pidgin &      # chat TODO: don't spawn buddy list
 dropboxd &    # file sync
+alsactl restore & # restore audio settings
 
 #:: Bind keys
 xbindkeys & # Media keys
@@ -10,4 +15,6 @@ xmodmap ~/.scripts/bind_keys & # swapping caps and control
 volumeicon & # Volume bindings
 
 #:: System specific
-~/.autostart.system.sh
+if [ -z $system ]; then
+  $system
+fi
