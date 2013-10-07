edcf0fccc7c21570c14d60086b54789d50546d8c
Caleb Everett <everett.caleb@gmail.com>
2013-10-07 17:30:40 -0400

    autostart: fix call to system auto start

--- home/.autostart.sh
+++ home/.autostart.sh
@@ -2,19 +2,19 @@
 
 system=~/.autostart.system.sh
 
+echo "Starting programs"
 #:: start programs
 xrandr --auto # screen setup
-compton &     # composition
-pidgin &      # chat TODO: don't spawn buddy list
+compton  &    # composition
+pidgin   &    # chat TODO: don't spawn buddy list
 dropboxd &    # file sync
 alsactl restore & # restore audio settings
 
 #:: Bind keys
 xbindkeys & # Media keys
-xmodmap ~/.scripts/bind_keys & # swapping caps and control
 volumeicon & # Volume bindings
 
 #:: System specific
-if [ -z $system ]; then
+if [ -x $system ]; then
   $system
 fi
