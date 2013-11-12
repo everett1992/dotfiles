c040e0441f8494a76cfac98647b81594a43f706c
Caleb Everett <everett.caleb@gmail.com>
2013-11-12 10:23:28 -0500

    dzen wifibar checks for device

--- home/.dzen/scripts/bar_wifi.sh
+++ home/.dzen/scripts/bar_wifi.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 QUAL=`iwconfig $1 | grep 'Link Quality=' | awk '{gsub(/[=/]/," "); print $3}'`
-MAX=`iwconfig $1 | grep 'Link Quality=' | awk '{gsub(/[=/]/," "); print $4}'`
-QUAL=${QUAL:-0}
-MAX=${MAX:-100}
-PERC=`echo $(($QUAL*100/$MAX))`
+if [ ! -z $QUAL ]; then
+  MAX=`iwconfig $1 | grep 'Link Quality=' | awk '{gsub(/[=/]/," "); print $4}'`
+  QUAL=${QUAL:-0}
+  MAX=${MAX:-100}
+  PERC=`echo $(($QUAL*100/$MAX))`
 
-if [[ $PERC -lt 20 ]]; then
-	ICON="wireless1.xbm"
-elif [[ $PERC -lt 40 ]]; then
-	ICON="wireless2.xbm"
-elif [[ $PERC -lt 60 ]]; then
-	ICON="wireless3.xbm"
-elif [[ $PERC -lt 80 ]]; then
-	ICON="wireless4.xbm"
-elif [[ $PERC -lt 101 ]]; then
-	ICON="wireless5.xbm"
+  if [[ $PERC -lt 20 ]]; then
+    ICON="wireless1.xbm"
+  elif [[ $PERC -lt 40 ]]; then
+    ICON="wireless2.xbm"
+  elif [[ $PERC -lt 60 ]]; then
+    ICON="wireless3.xbm"
+  elif [[ $PERC -lt 80 ]]; then
+    ICON="wireless4.xbm"
+  elif [[ $PERC -lt 101 ]]; then
+    ICON="wireless5.xbm"
+  fi
+
+  ICON='^i(/home/caleb/.dzen/icons/'"$ICON)"
+  echo "$color$ICON"
 fi
-
-ICON='^i(/home/caleb/.dzen/icons/'"$ICON)"
-echo "$color$ICON"
